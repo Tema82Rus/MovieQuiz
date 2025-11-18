c7cf06f170718696379632c5a4d565d33f88a2a2
Tema82Rus <Fenix23245@yandex.ru>
2025-11-18 16:11:28 +0300

    add Models and Services

---
+++ Models/QuizQuestion.swift
@@ -0,0 +1,14 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Artem Yaroshenko on 18.11.2025.
+//
+
+import Foundation
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
---
+++ Models/QuizResultsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Artem Yaroshenko on 18.11.2025.
+//
+
+import Foundation
+
+struct QuizResultsViewModel {
+    let title: String
+    let textScorePoints: String
+    let buttonText: String
+}
