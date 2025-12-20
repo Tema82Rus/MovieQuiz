7baf349805f1e6698aee4bf2173ff3acb2a45b8b
Tema82Rus <Fenix23245@yandex.ru>
2025-12-20 23:45:30 +0300

    feat(AlertPresenter): add AccessibilityIdentifier for AlertPresenter

---
+++ Models/AccessibilityIdentifier.swift
@@ -0,0 +1,12 @@
+//
+//  AccessibilityIdentifier.swift
+//  MovieQuiz
+//
+//  Created by Artem Yaroshenko on 18.12.2025.
+//
+
+import Foundation
+
+struct AccessibilityIdentifier {
+    static let gameResults = "Game results"
+}
